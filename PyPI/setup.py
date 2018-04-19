464bd5987ce6f7090e047db59c1b184d654f5b62
Tada0 <tomekholda@gmail.com>
2018-04-19 22:05:28 +0200

    adding dist files

---
+++ PyPI/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='tada0-toddler-neural-network',
+    version='1.0.4',
+    scripts=['neural_network.py', 'nn_exceptions.py'],
+    author='Tomasz Hołda',
+    description='Little neural network library',
+)
