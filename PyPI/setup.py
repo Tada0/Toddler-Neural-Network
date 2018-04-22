81125545325e76e14f4760d281b04851286fb149
Tada0 <tomekholda@gmail.com>
2018-04-22 19:30:36 +0200

    new version

--- PyPI/setup.py
+++ PyPI/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tada0-toddler-neural-network',
-    version='2.0.0',
+    version='2.0.4',
     url='https://github.com/Tada0/Toddler-Neural-Network',
     description='Little neural network library',
     author='Tomasz Hołda',
